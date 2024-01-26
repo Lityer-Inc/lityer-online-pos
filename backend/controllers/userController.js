baee5dd171508492c0be31a8cc2069b557185ba6
mohammadashrafulislam <ixdell41@gmail.com>
2024-01-26 20:48:25 +0600

    added login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,3 +32,28 @@ export const userRegister = async (req, res) =>{
         return res.status(500).json({ error: "Internal Server Error" });
       }
 }
+
+export const userLogin = async (req, res)=>{
+  const {email, password} = req.body;
+  try{
+   if(!email || !password){
+    return res.status(400).json({ error: "Required fields are missing." })
+   }
+   const user = await userModel.findOne({ email });
+  //  check if user is exist or not:
+  if(!user){
+    return res.status(400).json({error: "User not found"})
+  }
+  // Compare passwords
+  const isPasswordMatch = user.email === email;
+
+  if (!isPasswordMatch) {
+    return res.status(401).json({ error: "Invalid password" });
+  }
+  return res.status(200).json({ message: "Login successful" });
+  }
+  catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
